b269e587a94a6669f2c4ebdf4cbc25e46535776b
mainmeister <mainmeister@gmail.com>
2016-10-24 14:06:00 -0400

    Completed version 1

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import os
 import praw
+import time
 
 def toaster(title, message):
     cmd = 'notify-send "%s" "%s"' % (title, message)
     os.system(cmd)
 
+def reddit():
+    r = praw.Reddit('ThoughtsInShower by u/mainmeister ver 0.1 see https://github.com/mainmeister/showerthoughts for source')
+    processed_subs = []
+    while True:
+        subr = r.get_subreddit('Showerthoughts')
+        for message in subr.get_new():
+            if not message.id in processed_subs:
+                toaster(message.author.name, message.title)
+                processed_subs.append(message.id)
+        time.sleep(5)
+
 if __name__ == '__main__':
-    toaster("This is a test", "Hello World!")
+    reddit()
