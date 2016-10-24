7fd415dc2eae8f280ff39559b9fe77a88c8a2b1a
mainmeister <mainmeister@gmail.com>
2016-10-24 17:55:14 -0400

    Clean up startup

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ def toaster(title, message):
     os.system(cmd)
 
 def reddit():
-    r = praw.Reddit('ThoughtsInShower by u/mainmeister ver 0.1 see https://github.com/mainmeister/showerthoughts for source')
     processed_subs = []
+    r = praw.Reddit('ThoughtsInShower by u/mainmeister ver 0.1 see https://github.com/mainmeister/showerthoughts for source')
+    subr = r.get_subreddit('Showerthoughts')
+    messages = []
     while True:
-        subr = r.get_subreddit('Showerthoughts')
-        for message in subr.get_new():
-            if not message.id in processed_subs:
-                toaster(message.author.name, message.title)
-                processed_subs.append(message.id)
-        time.sleep(5)
+        if len(messages) == 0:
+            messages = subr.get_new()
+            for message in messages:
+                if not message.id in processed_subs:
+                    toaster(message.author.name, message.title)
+                    processed_subs.append(message.id)
+                    time.sleep(10)
+            messages = []
+        time.sleep(10)
 
 if __name__ == '__main__':
     reddit()
